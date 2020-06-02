62bdfb6655c27e0ba3ceddee6de2cd39426f9254
Abhishek Patel <5524161+Abhishek8394@users.noreply.github.com>
2020-06-02 00:00:04 -0700

    move data into threads

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,26 @@
 use std::thread;
 use std::time::Duration;
 
-// fn start_thread(name: String){
-//     thread::spawn(||{
-//         for i in 1..10{
-//             println!("number {} from {}", i, name);
-//             thread::sleep(Duration::from_millis(1));
-//         }
-//     });
-// }
-
-fn main() {
-    // start_thread(String::from("t1"));
-    // start_thread(String::from("t2"));
-    thread::spawn(||{
+fn start_thread(name: String) -> thread::JoinHandle<()>{
+    let handle =  thread::spawn(move ||{
         for i in 1..10{
-            println!("number {} from {}", i, "t1");
+            println!("number {} from {}", i, name);
             thread::sleep(Duration::from_millis(1));
         }
     });
+    /// below will error, we already moved name in closure above.
+    // drop(name);
+    return handle;
+}
+
+fn main() {
+    let t1 = start_thread(String::from("t1"));
+    let t2 = start_thread(String::from("t2"));
+    
     for i in 1..5{
         println!("{} from main thread", i);
         thread::sleep(Duration::from_millis(1));
     }
+    t2.join().unwrap();
+    t1.join().unwrap();
 }
